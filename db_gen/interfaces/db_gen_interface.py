1cf481569b90c364136458c520dd92ffda9a2fb0
borisalexeenko <boriskust01@gmail.com>
2022-03-05 19:11:00 +0300

    fix(db_gen): add NotImplementedError
    ref: [issue-3]

--- db_gen/interfaces/db_gen_interface.py
+++ db_gen/interfaces/db_gen_interface.py
@@ -7,14 +7,14 @@ class IDbGen:
     @abstractmethod
     def create_db_table(self, count_of_columns):
         """Create specified table in database"""
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def add_row_to(self, table_name, values):
         """Add values to specified table"""
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def save_db_to_file(self):
         """Save database to .db file"""
-        pass
+        raise NotImplementedError
