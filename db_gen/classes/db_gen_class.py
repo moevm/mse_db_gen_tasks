863a0ca1616cf855315d2624bbee538ecd2689b1
borisalexeenko <boriskust01@gmail.com>
2022-03-05 20:25:48 +0300

    feat(db_gen): added class db_gen
    ref: [issue-4]

--- db_gen/classes/db_gen_class.py
+++ db_gen/classes/db_gen_class.py
@@ -1,18 +1,33 @@
 from db_gen.interfaces.db_gen_interface import IDbGen
-import pydbgen
 from pydbgen import pydbgen
+import sqlite3
 
 
 class DbGen(IDbGen):
     def __init__(self, db_file_name):
-        self.db_gen = pydbgen.pydbgen()
+        self.conn = None
+        self.db_gen = pydbgen.pydb()
         self.db_file = db_file_name
 
     def create_db_table(self, table_name, count_of_rows, column_names):
         self.db_gen.gen_table(db_file=self.db_file, table_name=table_name, fields=column_names, num=count_of_rows)
 
     def add_row_to(self, table_name, values):
-        pass
+        self.conn = sqlite3.connect(self.db_file)
+        c = self.conn.cursor()
+        val = ''
+        j = 0
+        for i in values:
+            val += "'" + i + "'"
+            if j != len(values) - 1:
+                val += ', '
+            j += 1
+        print(val)
+        c.execute(f"INSERT INTO {table_name} VALUES ({val})")
+        self.conn.commit()
+        c.execute(f"select * from {table_name}")
+        print(c.fetchall())
+        self.save_db_to_file()
 
     def save_db_to_file(self):
-        pass
+        self.conn.commit()
