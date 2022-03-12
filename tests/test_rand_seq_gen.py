281dd8e099119c08fca38a3b2e34668310df59e6
Alexander Shunyaev <av.shunaev.qux@gmail.com>
2022-03-12 15:31:10 +0300

    fix(tests): fix rand seq gen tests
    ref: [issue#17]

--- tests/test_rand_seq_gen.py
+++ tests/test_rand_seq_gen.py
@@ -1,5 +1,4 @@
 import unittest
-import time
 from random_number_sequence_generator.random_num_seq_gen import RandomNumberSequenceGenerator
 
 
@@ -7,17 +6,31 @@ class TestRandomNumberSequenceGenerator(unittest.TestCase):
 
     def setUp(self):
         self.gen = RandomNumberSequenceGenerator()
+        self.gen.init_with_random_seed()
 
-    def test_random_in_sequence(self):
+    def test_numbers_is_int(self):
+        numbers = [self.gen.next(), self.gen.next(), self.gen.next(), self.gen.next()]
+        for i in range(0, len(numbers)):
+            with self.subTest(i=i):
+                self.assertEqual(type(numbers[i]), type(1))
+
+    def test_numbers_in_borders(self):
         # tests random nums in sequence
-        self.assertNotEqual(self.gen.next(), self.gen.next())
+        numbers = [self.gen.next(), self.gen.next(), self.gen.next(), self.gen.next()]
+        for i in range(0, len(numbers)):
+            with self.subTest(i=i):
+                self.assertGreaterEqual(numbers[i], self.gen.min_border)
+                self.assertLessEqual(numbers[i], self.gen.max_border)
 
     def test_same_seeds(self):
         # tests that nums in sequence with same seeds are equal
-        self.gen.init_with_seed(20)
-        tmp = self.gen.next()
-        self.gen.init_with_seed(20)
-        self.assertEqual(tmp, self.gen.next())
+        numbers = [self.gen.next(), self.gen.next(), self.gen.next(), self.gen.next()]
+        for i in range(0, len(numbers)):
+            with self.subTest(i=i):
+                self.gen.init_with_seed(numbers[i])
+                tmp = self.gen.next()
+                self.gen.init_with_seed(numbers[i])
+                self.assertEqual(tmp, self.gen.next())
 
     def test_different_seeds(self):
         # tests that nums in sequence with different seeds are not equal
@@ -26,10 +39,6 @@ class TestRandomNumberSequenceGenerator(unittest.TestCase):
         self.gen.init_with_seed(300)
         self.assertNotEqual(tmp, self.gen.next())
 
-    def test_random_seeds(self):
-        # tests that init_with_random_seed sets not equal seed
-        self.gen.init_with_random_seed()
-        tmp = self.gen.next()
-        time.sleep(1)
-        self.gen.init_with_random_seed()
-        self.assertNotEqual(tmp, self.gen.next())
+
+if __name__ == '__main__':
+    unittest.main()
