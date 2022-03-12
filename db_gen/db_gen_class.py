338a55dd83dfe2f60a3e0ec5ab129c4e45efda2a
Никита Пэтайчук <31537936+NikitaPetaichuk@users.noreply.github.com>
2022-03-12 16:37:45 +0300

    Revert "feat(db_gen): added descrbe method to db_gen class"

--- db_gen/db_gen_class.py
+++ db_gen/db_gen_class.py
@@ -4,7 +4,6 @@ import sqlite3
 
 class DbGen:
     """Class database generator"""
-
     def __init__(self, db_file_name):
         self.conn = None
         self.db_gen = pydbgen.pydb()
@@ -16,7 +15,13 @@ class DbGen:
     def add_row_to(self, table_name, values):
         self.conn = sqlite3.connect(self.db_file)
         c = self.conn.cursor()
-        val = "'" + "', '".join(values) + "'"
+        val = ''
+        j = 0
+        for i in values:
+            val += "'" + i + "'"
+            if j != len(values) - 1:
+                val += ', '
+            j += 1
         print(val)
         c.execute(f"INSERT INTO {table_name} VALUES ({val})")
         self.conn.commit()
@@ -26,17 +31,3 @@ class DbGen:
 
     def save_db_to_file(self):
         self.conn.commit()
-
-    def describe_db(self):
-        with self.conn:
-            self.conn = sqlite3.connect(self.db_file)
-            c = self.conn.cursor()
-            c.execute("SELECT name, sql FROM sqlite_master WHERE type='table' ORDER BY name;")
-            meta_data = c.fetchall()
-            table_names = [i[0][0] for i in meta_data]
-            for j in table_names:
-                c.execute(f"PRAGMA table_info({j})")
-                data = c.fetchall()
-                print(f"Таблица {j} включает в себя следующие столбцы:")
-                for d in data:
-                    print(f"\t{d[0] + 1}. Столбец {d[1]} типа {d[2]}")
