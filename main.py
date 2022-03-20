997287abe8f7e4c7f61c717b74ef25dfede26fdf
kirja1980 <kirja1980@yandex.ru>
2022-03-20 20:18:46 +0300

    add saveJSON in Tree

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from db_gen.db_gen_class import DbGen
+from db_tree.main import Tree
 
 
 def main():
     db_generator = DbGen("dataBaseFile.db")
-    db_generator.create_db_table("a", 5, ["name", "country"])
-    db_generator.create_db_table("b", 5, ['name', 'street_address', 'city', 'phone', 'email', 'year', 'date'])
-    db_generator.add_row_to("a", ["Boris", "Russia"])
-    db_generator.describe_db()
+    tree = Tree()
+    tree.loadJSON("save.json", db_generator)
+    tree.saveJSON("save1.json")
+
+    # db_generator.create_db_table("a", 5, ["name", "country"])
+    # db_generator.create_db_table("b", 5, ['name', 'street_address', 'city', 'phone', 'email', 'year', 'date'])
+    # db_generator.add_row_to("a", ["Boris", "Russia"])
+    # db_generator.describe_db()
 
 
 if __name__ == "__main__":
