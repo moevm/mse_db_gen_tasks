e1a8faa937bdd00c8791c371835baa8145423ead
XandrNiko <99441694+XandrNiko@users.noreply.github.com>
2022-03-26 14:29:11 +0300

    Add files via upload

--- tests/test_db_tree.py
+++ tests/test_db_tree.py
@@ -1,43 +1,43 @@
-import unittest
 import json
+import os
+import unittest
+
 from db_tree.db_tree_class import Tree
 
 
 class TestTree(unittest.TestCase):
 
+    def setUp(self):
+        self.tree = Tree()
+        with open('input_tmp.json', 'w') as file:
+            json.dump({"table1": {"name": "varchar(50)", "city": "varchar(50)"},
+                       "table2": {"name": "varchar(50)", "city": "varchar(50)"}}, file)
+
+        self.tree.loadJSON('input_tmp.json')
+        self.tree.saveJSON('output_tmp.json')
+
+        with open('input_tmp.json') as file:
+            self.inputFile = json.load(file)
+
+        with open('output_tmp.json') as file:
+            self.outputFile = json.load(file)
+
     def test_loadJSON(self):
-        tree = Tree()
-        tree.loadJSON('example.json')
-        tree.saveJSON('utesttree.json')
-
-        with open('example.json') as file:
-            inputFile = json.load(file)
-
-        with open('utesttree.json') as file:
-            outputFile = json.load(file)
-
-        self.assertTrue(inputFile == outputFile)
+        self.assertTrue(self.inputFile == self.outputFile)
 
     def test_saveJSON(self):
-        tree = Tree()
-        tree.loadJSON('example.json')
-        tree.saveJSON('utesttree.json')
-
-        with open('example.json') as file:
-            inputFile = json.load(file)
-
-        with open('utesttree.json') as file:
-            outputFile = json.load(file)
-
-        self.assertTrue(outputFile == inputFile)
+        self.assertTrue(self.outputFile == self.inputFile)
 
     def test_addNode(self):
-        tree = Tree()
+        childs_tmp = ['table3', 'table4', 'table5']
+        for child in childs_tmp:
+            self.tree.addNode(child)
+            self.assertEqual(child, self.tree.root.childs[-1])
 
-        childs = ['A', 'B', 'C']
-        for child in childs:
-            tree.addNode(child)
+    def tearDown(self):
+        os.remove('input_tmp.json')
+        os.remove('output_tmp.json')
 
-        self.assertTrue(len(childs) == len(tree.root.childs))
-        for i in range(0, len(childs)):
-            self.assertTrue(childs[i] == tree.root.childs[i])
+
+if __name__ == "__main__":
+    unittest.main()
