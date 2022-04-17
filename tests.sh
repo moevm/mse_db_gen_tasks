d184c20e946aa52acabc3235891b9a579e409ae0
kirja1980 <kirja1980@yandex.ru>
2022-04-17 11:29:16 +0300

    redone bash

--- tests.sh
+++ tests.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
 usage(){
-   echo "Usage: $0 [-h] [run_all] [run_static_all] [run_seq_gen_tests] [run_db_tests]"
+   echo "Usage: $0 [-h] [run_all [seed]|NULL] [run_seq_gen_tests [seed]|NULL] [run_db_tests [seed]|NULL]"
     echo "  -h  Help. Display this message and quit."
     echo "  run_all - Run all tests"
-    echo "  run_static_all - Run all static tests"
     echo "  run_seq_gen_tests - Run tests of random number sequence generator"
     echo "  run_db_tests - Run tests of database"
     exit
 }
-run_all(){
-  python3 -m unittest -v tests/test_*.py
-}
 
-run_static_all(){
-  python3 -m unittest -v tests/test_*.py "$1"
+run_all(){
+  if [ "$1" ]; then
+      python3 -m unittest -v tests/test_*.py "$1"
+      else python3 -m unittest -v tests/test_*.py
+  fi
 }
 
 run_seq_gen_tests(){
-  python3 -m unittest -v tests/test_rand*.py
+  if [ "$1" ]; then
+      python3 -m python3 -m unittest -v tests/test_rand*.py "$1"
+      else python3 python3 -m unittest -v tests/test_rand*.py
+  fi
   ls
   rm -f ./test_db_file.db
   rm -f ./US_Cities.txt
 }
 
 run_db_tests(){
-  python3 -m unittest -v tests/test_db*.py
+  if [ "$1" ]; then
+      python3 -m unittest -v tests/test_db*.py "$1"
+      else python3 -m unittest -v tests/test_db*.py
+  fi
 }
 
 remove_file(){
@@ -49,21 +54,16 @@ do
             exit 0
             ;;
         run_all)
-            run_all
-            remove_file
-            exit 0
-            ;;
-        run_static_all)
-            run_static_all "$1"
+            run_all "$1"
             remove_file
             exit 0
             ;;
         run_seq_gen_tests)
-            run_seq_gen_tests
+            run_seq_gen_tests "$1"
             exit 0
             ;;
         run_db_tests)
-            run_db_tests
+            run_db_tests "$1"
             remove_file
             exit 0
             ;;
